32cc04200d1a7acbf6293b86f9718b42291c7968
SiruoWang <sara.siruo@outlook.com>
2019-09-17 14:26:01 -0400

    update iap()

--- R/iap_v2.R
+++ R/iap_v2.R
@@ -1,4 +1,3 @@
-
 library(dplyr)
 library(broom)
 library(gam)
@@ -57,7 +56,7 @@ iap <- function(inf_formula, valid_dat, rel_model, bs = 100, seed = 1234){
       
       #sim_y = rbinom(ss, 1, prob=predict(rel_model, bs_data, type = "prob")[,2])
       
-      prob <- predict(rel_model, bs_data, type = "prob")
+      prob  <- predict(rel_model, bs_data, type = "prob")
       
       sim_y <- unlist(lapply(1:nrow(prob), function(x) {
         
@@ -67,46 +66,46 @@ iap <- function(inf_formula, valid_dat, rel_model, bs = 100, seed = 1234){
         }))
       
       
-      bs_data$sim_y <- as.factor(sim_y)
+      bs_data$sim_y   <- as.factor(sim_y)
       
-      sim_inf_formula = update(inf_formula, sim_y ~ .)
+      sim_inf_formula <- update(inf_formula, sim_y ~ .)
       
-      sim_inf_table = tidy(suppressWarnings(glm(sim_inf_formula, bs_data, family = binomial(link = "logit"))))
+      sim_inf_table   <- tidy(suppressWarnings(glm(sim_inf_formula, bs_data, family = binomial(link = "logit"))))
     }
   
     
     
-    term = sim_inf_table$term[-1]
+    term      <- sim_inf_table$term[-1]
     
-    estimate = sim_inf_table$estimate[-1]
+    estimate  <- sim_inf_table$estimate[-1]
     
-    std.error = sim_inf_table$std.error[-1]
+    std.error <- sim_inf_table$std.error[-1]
     
     ## check whether any covariate contains all 0s and don't have the inference results when fitting the inference model
-    match_cov = match(covariates,sim_inf_table$term[-1])
+    match_cov <- match(covariates,sim_inf_table$term[-1])
     
     if (anyNA(match_cov)){
       
-      na_cov = covariates[which(is.na(match_cov))]
+      na_cov   <- covariates[which(is.na(match_cov))]
       
-      term = c(term, na_cov)
+      term     <- c(term, na_cov)
       
-      estimate = c(estimate, rep(NA, length(na_cov)))
+      estimate <- c(estimate, rep(NA, length(na_cov)))
       
-      std.error = c(std.error, rep(NA, length(na_cov)))
+      std.error<- c(std.error, rep(NA, length(na_cov)))
       
     }
     
-    sim_table = data.frame(term, estimate, std.error)
+    sim_table <- data.frame(term, estimate, std.error)
     
-    sim_table = sim_table[match(covariates, sim_table$term),]
+    sim_table <- sim_table[match(covariates, sim_table$term),]
     
     
   }) ## end lapply
   
   
   ## calculate medians to each covariate's estimate, standeard error over bootstrap procedures
-  table_temp = lapply(1:length(covariates), function(cov){
+  table_temp <- lapply(1:length(covariates), function(cov){
     
     rowMedians(sapply(table_list, function(x) as.numeric(x[cov,-1])),
                na.rm = TRUE)
@@ -114,19 +113,19 @@ iap <- function(inf_formula, valid_dat, rel_model, bs = 100, seed = 1234){
   })
   
   ## create a tidy table similar to broom output with term, estimate, std.error, p.value
-  tidytable = data.frame(cbind(covariates, do.call(rbind,table_temp)))
+  tidytable           <- data.frame(cbind(covariates, do.call(rbind,table_temp)))
   
-  colnames(tidytable) = c("term", "estimate", "std.error")
+  colnames(tidytable) <- c("term", "estimate", "std.error")
   
-  tidytable$estimate = as.numeric(as.character(tidytable$estimate))
+  tidytable$estimate  <- as.numeric(as.character(tidytable$estimate))
   
-  tidytable$std.error = as.numeric(as.character(tidytable$std.error))
+  tidytable$std.error <- as.numeric(as.character(tidytable$std.error))
   
-  tidytable = na.omit(tidytable)
+  tidytable           <- na.omit(tidytable)
   
-  tidytable$statistic = tidytable$estimate / tidytable$std.error
+  tidytable$statistic <- tidytable$estimate / tidytable$std.error
   
-  tidytable$p.value = 2*pt(-abs(tidytable$statistic),df = ss -1 - length(covariates))
+  tidytable$p.value   <- 2*pt(-abs(tidytable$statistic),df = ss -1 - length(covariates))
   
   
   
@@ -139,47 +138,47 @@ iap_der <- function(inf_formula, outcome_name, prediction_name, valid_dat, test_
   
   if (is.numeric(test_dat[ , outcome_name])){
            
-       covariates = all.vars(inf_formula)[-1]
+       covariates      <- all.vars(inf_formula)[-1]
        
        ## inference formula for testing data with observed outcome
-       inf_formula_obs = as.formula(paste(outcome_name, "~", paste(covariates, collapse ="+")))
+       inf_formula_obs <- as.formula(paste(outcome_name, "~", paste(covariates, collapse ="+")))
        
        ## calculate bias on testing set
-       bias = tidy(lm(inf_formula, test_dat))$estimate[-1] - tidy(lm(inf_formula_obs, test_dat))$estimate[-1]
+       bias <- tidy(lm(inf_formula, test_dat))$estimate[-1] - tidy(lm(inf_formula_obs, test_dat))$estimate[-1]
        
-       yp_y = as.formula(paste0(outcome_name, " ~ ", prediction_name))
+       yp_y <- as.formula(paste0(outcome_name, " ~ ", prediction_name))
        
        ## calculate conditional variance of yp on testing set
-       gamma1 = tidy(lm(yp_y, test_dat))$estimate[-1]
+       gamma1   <- tidy(lm(yp_y, test_dat))$estimate[-1]
        
-       sigma_p = glance(lm(yp_y, test_dat))$sigma
+       sigma_p  <- glance(lm(yp_y, test_dat))$sigma
        
-       sigma_y = glance(lm(inf_formula, test_dat))$sigma
+       sigma_y  <- glance(lm(inf_formula, test_dat))$sigma
        
-       cond_var = sigma_p + gamma1^2 * sigma_y
+       cond_var <- sigma_p + gamma1^2 * sigma_y
        
        
        ## derivation correction of iap estimate on validation set
-       estimate = tidy(lm(inf_formula, valid_dat))$estimate[-1] - bias
+       estimate <- tidy(lm(inf_formula, valid_dat))$estimate[-1] - bias
        
        ## derivation correction of iap standard error on validation set
-       design_matrix = cbind(rep(1, nrow(valid_dat)), valid_dat[, covariates])  %>% as.matrix()
+       design_matrix <- cbind(rep(1, nrow(valid_dat)), valid_dat[, covariates])  %>% as.matrix()
        
-       var_matrix = solve(t(design_matrix) %*% design_matrix) * cond_var
+       var_matrix    <- solve(t(design_matrix) %*% design_matrix) * cond_var
        
-       std.error = sqrt(diag(var_matrix)[-1])
+       std.error     <- sqrt(diag(var_matrix)[-1])
        
        ## calcute t-statistic and p-value
-       statistic = estimate/std.error
+       statistic  <- estimate/std.error
        
-       p.value = 2*pt(-abs(statistic), df = nrow(valid_dat) - 1 - length(covariates))
+       p.value    <- 2*pt(-abs(statistic), df = nrow(valid_dat) - 1 - length(covariates))
        
-       tidytable = data.frame(term = covariates, 
-                              estimate = estimate,
-                              std.error = std.error, 
-                              statistic = statistic,
-                              p.value = p.value,
-                              row.names = NULL)
+       tidytable  <- data.frame(term = covariates, 
+                               estimate = estimate,
+                               std.error = std.error, 
+                               statistic = statistic,
+                               p.value = p.value,
+                               row.names = NULL)
      }
   
   
@@ -270,5 +269,6 @@ tidy(glm(formula, validation, family = binomial(link = "logit")))
 ## show the inference results on validation set without corrections using observed outcomes 
 ## This output is only for comparing results. In practice we do not have observed outcomes on validation set.
 tidy(glm(update(formula, actual ~ .), validation, family = binomial(link = "logit")))
+
 
 
