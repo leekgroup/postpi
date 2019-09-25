3de9fbf54c88556a29411cc85da0c9031c33ad5f
SiruoWang <sara.siruo@outlook.com>
2019-09-25 13:05:05 -0400

    update documentation for postpi_relate function

--- R/postpi_relate.R
+++ R/postpi_relate.R
@@ -1,12 +1,22 @@
-#' postpi_relate function
 #'
-#' This function models the relationship between the observed and predicted outcomes.
+#' postpi_relate function models the the relationship between the observed and predicted outcomes.
+#'
+#' This function takes in a data set (i.e. testing set) containing only observed and predicted outcomes.
+#' It relates observed and predicted continuous outcomes through a gamma function.
+#' For categorical outcomes, user inputs observed outcomes and the probabilities of predicted outcomes (i.e. probabilities for each predicted category).
+#' It related categorical data through a user defined maching learning method from the caret package. The default method is k-nearest neighbours.
+#'
+#' @param yobs name of the observed outcome in the testing set
+#' @param test_dat testing set that contains observed outcomes and predicted outcomes (continuous data) or probabilities of predicted outcomes (categorical data)
+#' @param yobs name of the observed outcome in the testing set
+#'
+#' @return rel_model relationship model between observed outcomes and predicted outcomes/probabilities
+#'
 #' @import gam
 #' @import caret
 #'
-#' @param test_dat testing set that contains observed outcomes and predicted outcomes (continuous data) or probabilities of predicted outcomes (categorical data)
-#' @param yobs name of the observed outcome in the testing set
 #' @export
+#'
 postpi_relate <- function(test_dat, yobs, method = "knn"){
 
   yobs  <- deparse(substitute(yobs))
