f6a9b2557956bfea7400d31d2ed913ceaf39f7e7
SiruoWang <sara.siruo@outlook.com>
2019-10-02 13:29:07 -0400

    update on vignettes

--- doc/vignettes.R
+++ doc/vignettes.R
@@ -17,13 +17,13 @@ data_split <- rsample::initial_split(data, prop = 1/2)
 testing    <- rsample::training(data_split)
 validation <- rsample::testing(data_split)
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## fit the relationship model on testing set
 rel_model <- testing %>%
   select(actual, predictions) %>%
   postpi_relate(actual)
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 inf_formula <- predictions ~ region_10
 
 ## fit the inference model on validation set and make iap corrections using bootstrap approach
@@ -31,12 +31,12 @@ results_postpi <- validation %>%
   postpi(rel_model, inf_formula)
 
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## fit the inference model on validation set and make iap corrections using derivation approach
 results_der <- testing %>%
   postpi_der(actual, predictions, validation, inf_formula)
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## show the inference results on validation set using observed outcomes
 broom::tidy(lm(update(inf_formula, actual ~ .), validation))[-1,] %>%
   kable() %>%
@@ -44,19 +44,19 @@ broom::tidy(lm(update(inf_formula, actual ~ .), validation))[-1,] %>%
   column_spec(3, bold = T, color = "red") %>%
   column_spec(4, bold = T, color = "blue")
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 kable(results_postpi) %>%
   kable_styling("striped", full_width = F) %>%
   column_spec(3, bold = T, color = "red") %>%
   column_spec(4, bold = T, color = "blue")
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 kable(results_der) %>%
   kable_styling("striped", full_width = F) %>%
   column_spec(3, bold = T, color = "red") %>%
   column_spec(4, bold = T, color = "blue")
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## show the inference results on validation set without corrections
 broom::tidy(lm(inf_formula, validation))[-1,] %>%
   kable() %>%
@@ -64,7 +64,7 @@ broom::tidy(lm(inf_formula, validation))[-1,] %>%
   column_spec(3, bold = T, color = "red") %>%
   column_spec(4, bold = T, color = "blue")
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## fit the relationship model on testing set
 rel_model <- testing %>%
   select(actual, predictions) %>%
@@ -80,7 +80,7 @@ results_der <- testing %>%
   postpi_der(actual, predictions, validation, inf_formula)
 
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## gold standard
 broom::tidy(lm(update(inf_formula, actual ~ .), validation))[-1,] %>%
   kable() %>%
@@ -129,13 +129,13 @@ data_split <- rsample::initial_split(TISSUE_data, prop = 1/2)
 testing    <- rsample::training(data_split)
 validation <- rsample::testing(data_split)
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE, warning=FALSE---------------------------------------
 # fit the relationship model on testing set
 rel_model <- testing %>%
   select(actual, Adipose_Tissue, Breast) %>%
   postpi_relate(actual)
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 inf_formula <- predictions ~ region_200
 
 ## fit the inference model on validation set and make iap corrections using bootstrap approach
@@ -143,7 +143,7 @@ results_postpi <- validation %>%
   postpi(rel_model, inf_formula)
 
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## show the inference results on validation set using observed outcomes
 broom::tidy(glm(update(inf_formula, actual ~ .), validation, family = binomial(link = "logit")))[-1,] %>%
   kable() %>%
@@ -151,13 +151,13 @@ broom::tidy(glm(update(inf_formula, actual ~ .), validation, family = binomial(l
   column_spec(3, bold = T, color = "red") %>%
   column_spec(4, bold = T, color = "blue")
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 kable(results_postpi) %>%
   kable_styling("striped", full_width = F) %>%
   column_spec(3, bold = T, color = "red") %>%
   column_spec(4, bold = T, color = "blue")
 
-## ------------------------------------------------------------------------
+## ---- message=FALSE------------------------------------------------------
 ## show the inference results on validation set without corrections
 broom::tidy(glm(inf_formula, validation, family = binomial(link = "logit")))[-1,] %>%
   kable() %>%
